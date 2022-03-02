cb9a674d7ca161be87c8ac8cad32256469e562f6
Saksham <saksham.kathuria0211@gmail.com>
2022-03-02 10:04:02 +0530

    Added links href and functions

--- client/src/components/Homepage/HomePage.jsx
+++ client/src/components/Homepage/HomePage.jsx
@@ -18,22 +18,16 @@ export default function Homepage() {
       const currentScrollY = window.scrollY;
       if (preScrollY.current < currentScrollY && goingUp) {
         setgoingUp(false);
-        // text.classList.add('dance');
       }
       if (preScrollY.current < currentScrollY && !goingUp) {
         setgoingUp(true);
-        // text.classList.remove('dance');
       }
       preScrollY.current = currentScrollY;
       let value = window.scrollY;
       animate.style.top = value * 2.5 + "px";
       animate.style.opacity = 1 - value / 50;
-      animate.style.zIndex = 1;
-      // animate.style.transitionTimingFunction = "ease-in-out";
       animate.style.transition = "0.7s";
-      value > 0 ? btn.style.visibility = "hidden" : btn.style.visibility = "visible";
-      
-      value>0? text.classList.remove('dance'):text.classList.add('dance');
+      value>0?animate.style.zIndex = 1:animate.style.zIndex = 20;
     };
     window.addEventListener("scroll", handleScroll);
 
@@ -41,7 +35,6 @@ export default function Homepage() {
   }, [goingUp]);
   let animate = document.getElementById("animate");
   let text = document.getElementById("text");
-  let btn = document.getElementById("btn");
   window.addEventListener("scroll", function () { });
 
   const countdown = () => {
@@ -80,7 +73,13 @@ export default function Homepage() {
       clearInterval()
     };
   })
-  
+  let vid = document.getElementById("popup");
+  function Show() {
+    vid.style.visibility = 'visible';
+  }
+  function Hide() {
+    vid.style.visibility = 'hidden';
+  }
   return (
     <div className="Main">
       <div id="animate">
@@ -88,19 +87,23 @@ export default function Homepage() {
           <div id='links' className="dance">
             <div id='youtube'>
               <img src={play} className="icons"/>
-              <a className='hov' href=''><span>Watch Teaser</span></a>
+              <a className='hov' href='javascript:void(0)' onClick={Show}><span className="linktxt">Watch Teaser</span></a>
             </div>
             <div id='reminder'>
               <img src={calender} className="icons"/>
-              <a className='hov' href=''><span>March 19</span></a>
+              <a className='hov' href='#' ><span className="linktxt">March 19</span></a>
             </div>
             <div id='discord'>
               <img src={discord} className="icons"/>
-              <a className='hov' href=''><span>Join Discord</span></a>
+              <a className='hov' href='https://discord.gg/ASDahbGy'><span className="linktxt">Join Discord</span></a>
             </div>
           </div>
           <span id="text" className="dance">E-SUMMIT</span>
         </div>
+      </div>
+      <div id="popup">
+        <iframe width="40%" height="100%" src="https://www.youtube.com/embed/65SudyBdaxk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <button id="close" onClick={Hide}>X</button>
       </div>
       <div id='btndiv' className="">
           <div id="countdown">
