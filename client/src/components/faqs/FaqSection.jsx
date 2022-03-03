f95b59f783d7ec7bb5659ba8dc1a18a46dfb4466
bhuvan-b <bokkabhuvan2@gmail.com>
2022-03-03 15:08:15 +0530

    Minor changes

--- client/src/components/faqs/FaqSection.jsx
+++ client/src/components/faqs/FaqSection.jsx
@@ -7,14 +7,16 @@ export default function FaqSection() {
   const [questions] = useState(data);
 
   return (
-    <div className="p-5 bg-[#000A0C]" id="faqs">
-      <div className="p-2 text-center font-Montserrat font-bold text-4xl md:text-6xl">
-        <span className="text-white">FAQ</span>
-        <span className="text-cyan-400">S </span>
+    <div className="p-4 flex flex-col justify-center items-center">
+      <div className="sm:w-3/4 w-[85%] bg-[#000A0C]" id="faqs">
+        <div className="p-2 pb-8 text-center font-Montserrat font-bold text-4xl md:text-6xl">
+          <span className="text-white">FAQ</span>
+          <span className="text-cyan-400">S </span>
+        </div>
+        {questions.map((question) => (
+          <SingleQuestion {...question} key={question.id} />
+        ))}
       </div>
-      {questions.map((question) => (
-        <SingleQuestion {...question} key={question.id} />
-      ))}
     </div>
   );
 }
