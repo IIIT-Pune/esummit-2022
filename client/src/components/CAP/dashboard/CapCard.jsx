6fa818ea48ce034a1fa550bd467d713ac0591797
bhuvan-b <bokkabhuvan2@gmail.com>
2022-03-07 15:48:36 +0530

    Modified leaderboard

--- client/src/components/CAP/dashboard/CapCard.jsx
+++ client/src/components/CAP/dashboard/CapCard.jsx
@@ -18,10 +18,10 @@ const CapCard = (props) => {
     }
   };
   return (
-    <div className="w-full lg:w-fit">
-      <div className="justify-center flex">
+    <div className="flex flex-col items-center  pt-6 lg:w-fit">
+      <div className="w-[90%] justify-center items-center flex">
         <div className="border border-sky-500 bg-black space-y-5 lg:space-y-10 w-full lg:w-4/5 flex flex-col justify-center items-center">
-          <div className="font-Montserrat text-3xl text-white font-bold mt-4 lg:my-2 lg:mt-8">
+          <div className="font-Montserrat text-2xl text-white font-bold mt-4 lg:my-2 lg:mt-8">
             <span className="text-sky-500 italic">HELLO, </span>
             <span>{props.name}</span>
           </div>
@@ -29,13 +29,13 @@ const CapCard = (props) => {
           <div>
             <div className="font-Poppins text-white font-bold grid grid-cols-2 gap-32 lg:gap-12 content-center items-center justify-center ">
               <div>
-                <div className="text-sky-500 text-center text-5xl">
+                <div className="text-sky-500 text-center text-2xl">
                   #{props.rank}
                 </div>
                 <div className="text-center">POSITION</div>
               </div>
               <div>
-                <div className="text-sky-500 text-center text-5xl">
+                <div className="text-sky-500 text-center text-2xl">
                   {props.points}
                 </div>
                 <div className="text-center">POINTS</div>
@@ -43,7 +43,7 @@ const CapCard = (props) => {
             </div>
           </div>
 
-          <div className="flex flex-col pb-4 lg:pb-0 justify-center items-center">
+          <div className="flex flex-col pb-4 lg:pb-0  justify-center items-center">
             <div className="font-Poppins text-white font-bold m-1">
               YOUR REFERRAL CODE
             </div>
